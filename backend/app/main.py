72d43393b665dd46474ce885cd493db0cd79ea78
KAAMI777 <pramitghose777@gmail.com>
2026-08-28 09:50:58 +0530

    k6

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,20 +4,16 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# frontend_url = os.getenv("FRONTEND_URL")
 
-origins = [
-    "http://localhost:5173",
-    "https://job-handler-4aoavbxig-kaami777.vercel.app",
-    "https://job-handler.vercel.app/"
-]
 
-# if frontend_url:
-#     origins.append(frontend_url)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://job-handler.vercel.app",
+    ],
+    allow_origin_regex=r"https://job-handler-.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

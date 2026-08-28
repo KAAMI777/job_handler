6e46395d08955c527ab90f8b80fcb4b5eaa01ac3
KAAMI777 <pramitghose777@gmail.com>
2026-08-28 09:44:41 +0530

    k5

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,8 @@ app = FastAPI()
 
 origins = [
     "http://localhost:5173",
-    "https://job-handler-4aoavbxig-kaami777.vercel.app"
+    "https://job-handler-4aoavbxig-kaami777.vercel.app",
+    "https://job-handler.vercel.app/"
 ]
 
 # if frontend_url:

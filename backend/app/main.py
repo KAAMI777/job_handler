453ed848c3879fd3b5fdea19e30fcd157980af0b
KAAMI777 <pramitghose777@gmail.com>
2026-08-28 07:10:08 +0530

    done some changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,26 @@
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
-frontend_url = os.getenv("VERCEL_URL")
-origins= ["http://localhost:5173/" , frontend_url]
+
+frontend_url = os.getenv("FRONTEND_URL")
+
+origins = [
+    "http://localhost:5173",
+]
+
+if frontend_url:
+    origins.append(frontend_url)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 @app.get("/health")
 def health():
-    return {"status": "pramit its working fine bro"}
+    return {"status": "ok"}
